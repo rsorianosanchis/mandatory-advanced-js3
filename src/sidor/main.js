9b43d327e04e4df204f815292e3b55d4c63e49af
rsoriano <rsorianosanchis@gmail.com>
2019-04-12 17:12:24 +0200

    fredag 12/04/2019-- 17:12

--- src/sidor/main.js
+++ src/sidor/main.js
@@ -1,15 +1,28 @@
 import React,{Component} from 'react';
+import axios from 'axios';
 import {Header} from '../labbComponents/header.js';
 import {Register} from './register.js';
 import {Login} from './login.js';
 import {TodoLista} from './todoLista.js';
 
+const API_ROOT= 'http://ec2-13-53-32-89.eu-north-1.compute.amazonaws.com:3000';
 export class Main extends Component {
+  constructor(props){
+    super(props)
+
+
+  }
+
+  // communication to backend
+  _usrRegister = (email,password)=>{
+    axios.post({ + '/resgister',{email, password})
+
+  }
   render(){
     return(
       <>
       <Header usrMail='pepito'/>
-      <Login/>
+      <Register/>
       </>
     )
   }
--- src/sidor/register.js
+++ src/sidor/register.js
@@ -1,10 +1,62 @@
 import React,{Component} from 'react';
-import {Header} from '../labbComponents/header.js';
+import jwt from 'jsonwebtoken';
 
 export class Register extends Component {
+  state = {
+    usrMail: '',
+    password: ''};
+
+  _handleLoginSumbit=(e)=>{
+    e.preventDefault();
+    console.log('tetsting ');
+    console.log(this.state.usrMail,this.state.password);
+  }
+  
+
+  _createToken = ()=>{
+    const token = jwt.sign({a:10},'secret');
+    console.log(token);
+  }
+
   render(){
     return(
       <React.Fragment>
+        <form onSubmit={this._handleLoginSumbit}>
+          <div className="field">
+            <p className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="email"
+                placeholder="Email"
+                onChange={(e)=>(this.setState({usrMail: e.target.value}))}/>
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                onChange={e=>{this.setState({password: e.target.value})}}/>
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control">
+              <button className="button is-success">
+                Submit
+              </button>
+            </p>
+          </div>
+        </form>
       </React.Fragment>
     )
   }
